b6217de372f91dad1e8959089b723e2c10bf99d3
kleesbomb <leamshe0@gmail.com>
2021-10-21 00:17:35 +0800

    Update_1 index

---
+++ Formative_5/index.php
@@ -0,0 +1,43 @@
+<!-- INDEX OF ALL OTHER PROGRAMS -->
+
+<!DOCTYPE html>
+
+<html lang = 'en' xmlns = "http://www.w3.org/1999/xthml">
+<head>
+
+    <!-- STYLE SHEET -->
+	<link rel="stylesheet" href="indexStyle.css" />
+	<link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet'>
+    <title>TA1 Index</title>
+</head>
+
+	<body>
+		<div class = "box">
+			<div class = "header">
+				<h1> INDEX FOR FORMATIVE 5  </h1> 
+
+				<p> by: <br><br>
+					Kyle Sebastian Claveria --- Junelle Espero --- Charles Dmitri Peñaranda --- Ella Fritz Sarmiento  </p>
+			</div> <!-- eof header -->
+
+			<hr>
+			<div class = "buttons">
+				<button type = "button" class = "btn"> <a href="FA5_1A.php"> FA5 - 1A</a> </button>
+
+				<br>
+
+				<button type = "button" class = "btn"> <a href = "FA5_1B.php"> FA5 - 1B</a> </button>
+
+				<br>
+
+				<button type = "button" class = "btn"> <a href = "Formative_3/FA3_3.php"> FA5 - 2 </a> </button>
+
+				<br>
+
+				<button type = "button" class = "btn"> <a href = "Formative_4/Num1/Homepage.php">FA5 - 3</a> </button>
+
+			</div> <!-- eof buttons -->
+
+		</div> <!-- eof box -->
+	</body>
+</html>
