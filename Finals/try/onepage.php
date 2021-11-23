8273e14b63be426259a01fa4fa5d8560df84d3d5
kleesbomb <leamshe0@gmail.com>
2021-11-23 22:47:43 +0800

    Update Scrollable One-page

---
+++ Finals/try/onepage.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Business Page</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index-style.css">
+    <link rel="stylesheet" href="onepagestyle.css"/>
+    <script src="https://kit.fontawesome.com/0c95e134ce.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+	<section id = "home">
+        <div class="navbar">
+            <header>
+            <a href = "#"> <img src="images/Logo.png" alt = "logo" width="80px"></a>
+            </header>
+
+            <ul id = "menubar">
+                <li> <a href="#home">Home</a> </li>
+                <li> <a href="#about">About Us</a> </li>
+                <li> <a href="#products">Products</a> </li>
+                <li> <a href="#customers">Customers</a> </li>
+                <li> <a href="#contact">Contact Us</a> </li>
+                <li> <a href="">Login</a> </li>
+                <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+            </ul>
+
+            
+        </div>
+
+        <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
+ <div class="animate__animated animate__backInLeft"><h1 >Lorem</h1></div>
+          <div class="animate__animated animate__backInLeft"><h1>Ipsum</h1></div>
+          <div class="animate__animated animate__backInLeft">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptates ullam neque aspernatur velit dolore at aliquid est sequi. Nemo architecto.</p>
+          </div>
+          <a href="Pages/Registration.php">
+            <div class="animate__animated animate__bounceIn animate__delay-2s">
+              <div class="button">Register Now</div>
+            </div>
+          </a>
+        
+    </section> 
+    <!-- eof home section -->
+
+    <section id = "about">
+       <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
+       <h1>about</h1>
+    </section>
+    <!-- eof about us section -->
+
+	<section id = "products">
+        <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
+        <h1>products</h1>     
+    </section> 
+    <!-- eof products section -->
+
+	<section id = "customers">
+      <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
+      <h1 style = "color: black">customers</h1>           
+    </section>
+    <!-- eof customers section -->
+
+    <section id = "contact">
+         <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
+         <h1>contact</h1>
+    </section>
+    <!-- eof contact us section -->
+</body>
+
+<footer>
+    <h3>Contact Us</h3>
+</footer>
+</html>
