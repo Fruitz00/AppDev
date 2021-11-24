fa563abb029221c81ec8f46c8d75caeffad3ab17
kleesbomb <leamshe0@gmail.com>
2021-11-25 00:07:39 +0800

    Update 2 Scrollable One-page

--- Finals/try/onepage.php
+++ Finals/try/onepage.php
@@ -7,14 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index-style.css">
     <link rel="stylesheet" href="onepagestyle.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
     <script src="https://kit.fontawesome.com/0c95e134ce.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
 
-	<section id = "home">
+    <section id = "home">
         <div class="navbar">
             <header>
             <a href = "#"> <img src="images/Logo.png" alt = "logo" width="80px"></a>
@@ -26,49 +27,151 @@
                 <li> <a href="#products">Products</a> </li>
                 <li> <a href="#customers">Customers</a> </li>
                 <li> <a href="#contact">Contact Us</a> </li>
-                <li> <a href="">Login</a> </li>
+                <li> <a href="../Pages/Log_In.php">Login</a> </li>
                 <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
             </ul>
 
             
         </div>
 
-        <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
- <div class="animate__animated animate__backInLeft"><h1 >Lorem</h1></div>
-          <div class="animate__animated animate__backInLeft"><h1>Ipsum</h1></div>
-          <div class="animate__animated animate__backInLeft">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptates ullam neque aspernatur velit dolore at aliquid est sequi. Nemo architecto.</p>
-          </div>
-          <a href="Pages/Registration.php">
-            <div class="animate__animated animate__bounceIn animate__delay-2s">
-              <div class="button">Register Now</div>
-            </div>
-          </a>
-        
+        <br><br><br><br><br><br><br><br><br> 
+
+        <div class = "home-content">
+            <div class="animate__animated animate__backInLeft"><h1 >Lorem</h1></div>
+              <div class="animate__animated animate__backInLeft"><h1>Ipsum</h1></div>
+              <div class="animate__animated animate__backInLeft">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptates ullam neque aspernatur velit dolore at aliquid est sequi. Nemo architecto.</p>
+              </div>
+              <a href="../Pages/Registration.php">
+                <div class="animate__animated animate__bounceIn animate__delay-2s">
+                  <div class="button">Register Now</div>
+                </div>
+
+              </a>
+        </div>
     </section> 
     <!-- eof home section -->
 
     <section id = "about">
-       <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
+        <br><br><br><br><br><br><br><br><br> 
        <h1>about</h1>
     </section>
     <!-- eof about us section -->
 
-	<section id = "products">
-        <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
-        <h1>products</h1>     
-    </section> 
-    <!-- eof products section -->
+    <!-- start of section-products -->
+    <section id = "products">
+    <div class="products-content">
+      <h2 align = "center">Featured Products</h2>
+      <br>
 
-	<section id = "customers">
-      <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+          <div class="col">
+              <img src="../Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+              <h4>Zhongli</h4>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <h6>₱100.00</h6>
+              <h6>Add to Cart</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+    <!-- end of section-products -->
+
+    <section id = "customers">
+        <br><br><br><br><br><br><br><br><br> 
       <h1 style = "color: black">customers</h1>           
     </section>
     <!-- eof customers section -->
 
     <section id = "contact">
-         <br> <br> <br> <br> <br> <br> <br> <br><br> <br> <br> <br> <br> <br>
-         <h1>contact</h1>
+        <br><br><br><br><br><br><br><br><br>         
+        <h1>contact</h1>
     </section>
     <!-- eof contact us section -->
 </body>
