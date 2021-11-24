8ea0af071f16e84a2235dbc6fd7b4f20ccc0f25f
Mallows <penarandacharlesdmitri@gmail.com>
2021-11-25 07:13:30 +0800

    Admin concept done
    need more fix

---
+++ Finals/Pages/Admin/admin.php
@@ -0,0 +1,50 @@
+<?php include 'adminheader.php'?>
+<body>
+    <div class="head">
+        <h1>Welcome</h1>
+    </div>
+    <div class="vl">
+		
+	</div>
+    <nav class="navbar navbar-fixed-left navbar-minimal animate" role="navigation">
+		<div class="navbar-toggler animate">
+        <script src="nav.js"></script>
+			<span class="menu-icon"></span>
+		</div>
+		<ul class="navbar-menu animate">
+			<li>
+				<a href="admin.php" class="animate">
+					<span class="desc animate"> Accounts </span>
+					<span class="glyphicon glyphicon-user"></span>
+				</a>
+			</li>
+			<li>
+				<a href="manage.php" class="animate">
+					<span class="desc animate"> Manage Product </span>
+					<span class="glyphicon glyphicon-info-sign"></span>
+				</a>
+			</li>
+			<li>
+				<a href="orders.php" class="animate">
+					<span class="desc animate"> Order List</span>
+					<span class="glyphicon glyphicon-comment"></span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+
+    <!-- Db needed to display all columns -->
+	<div class="disptable">
+		<table>
+			<tr>
+				<td> id </td>
+				<td> name </td>
+				<td> email </td>
+				<td> password</td>
+			</tr>
+		</table>	
+	</div>
+
+
+</body>
+</html>
---
+++ Finals/Pages/Admin/adminheader.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title> admin </title>
+    <!-- bootstrap -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link rel = "stylesheet" href = "admin.css">
+<header>
