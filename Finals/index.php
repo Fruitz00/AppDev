a9391123fc3a3ffc4cc480f8ff6293f21e083958
kleesbomb <leamshe0@gmail.com>
2021-11-30 16:02:22 +0800

    Updated index
    gumawa ulit ako ng css kasi hindi gumagana pag yung una yung stylesheet. Inaayos pa yung mga tinatry ko sa ibang section

--- Finals/index.php
+++ Finals/index.php
@@ -7,169 +7,252 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index-style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="css/indexStyle.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
     <script src="https://kit.fontawesome.com/0c95e134ce.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
-    <!-- menubar -->
-    <div class="menubar">
-      <div>
-        <img src="Photos/Logo.png" alt="Logo" width="80px">
-      </div>
-        <nav>
-          <ul>
-            <br>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Customers</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="Pages/Log_In.php">Login</a></li>
-            <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
-          </ul>
-        </nav>
-    </div>
-    <!-- end of menu bar -->
-    
-    <!-- start of section-intro -->
-    <div class="section-intro">
-      <div class="row">
-        <div class="col-2">
-          <br><br><br><br><br><br><br><br>
-          <div class="animate__animated animate__backInLeft"><h1>Lorem</h1></div>
-          <div class="animate__animated animate__backInLeft"><h1>Ipsum</h1></div>
-          <div class="animate__animated animate__backInLeft">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptates ullam neque aspernatur velit dolore at aliquid est sequi. Nemo architecto.</p>
-          </div>
-          <a href="Pages/Registration.php">
-            <div class="animate__animated animate__bounceIn animate__delay-2s">
-              <div class="button">Register Now</div>
-            </div>
-          </a>
-          <br>
-        </div>
-        <div class="col-2">
-          
-        </div>
-      </div>
-    </div>
-    <!-- end of section-intro  -->
-    
     <!-- start of section-products -->
-    <div class="products">
-      <br><br>
-      <h2 align = "center">Featured Products</h2>
-      <br>
+    <section id = "home">
+        <div class="navbar">
+            <header>
+            <a href = "#home"> <img src="Photos/Logo.png" alt = "logo" width="80px"></a>
+            </header>
 
-      <div class="container">
-        <div class="row">
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
-          </div>
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
-          </div>
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
-          </div>
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
-          </div>
+            <ul id = "menubar">
+                <li> <a href="#home">Home</a> </li>
+                <li> <a href="#about">About Us</a> </li>
+                <li> <a href="#products">Products</a> </li>
+                <li> <a href="#customers">Customers</a> </li>
+                <li> <a href="#contact">Contact Us</a> </li>
+                <li> <a href="Pages/Log_In.php">Login</a> </li>
+                <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+            </ul>
+
+            
         </div>
 
-        <div class="row">
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
-          </div>
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
-          </div>
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
-          </div>
-          <div class="col">
-              <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
-              <h4>Zhongli</h4>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <h6>₱100.00</h6>
-              <h6>Add to Cart</h6>
+        <br><br><br><br><br><br><br><br><br> 
+
+        <div class = "home-content">
+            <div class="animate__animated animate__backInLeft"><h1 >Lorem</h1></div>
+              <div class="animate__animated animate__backInLeft"><h1>Ipsum</h1></div>
+              <div class="animate__animated animate__backInLeft">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptates ullam neque aspernatur velit dolore at aliquid est sequi. Nemo architecto.</p>
+              </div>
+              <a href="Pages/Registration.php">
+                <div class="animate__animated animate__bounceIn animate__delay-2s">
+                  <div class="button">Register Now</div>
+                </div>
+
+              </a>
+        </div>
+    </section> 
+    <!-- eof section-home -->
+
+
+    <!-- start of section-about -->
+    <section id = "about">
+        <br><br><br><br><br><br><br><br><br> 
+        <h1>about</h1>
+    </section>
+    <!-- end of section-about  -->
+
+
+    <!-- start of section-products -->
+    <section id = "products">
+        <div class="products-content">
+          <h2 align = "center">Featured Products</h2>
+          <br>
+
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+
+              <div class="col">
+                  <img src="Photos/Products/Zhongli.png" width="150px" alt="Product 1" align = "center">
+                  <h4>Zhongli</h4>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <h6>₱100.00</h6>
+                  <h6>Add to Cart</h6>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+    </section>
     <!-- end of section-products -->
 
-    <!-- start of section-customer -->
-    <div class="about-us">
-      
 
-    </div>
-    <!-- end of section-customer -->
+    <!-- start of section-customers -->
+    <section id = "customers">
+        <br><br><br><br><br><br><br><br><br> 
+        <h1 style = "color: black">customers</h1>           
+    </section>
+    <!-- end of section-customers  -->
+
+    <section id = "contact">   
+        <div class = "contact-content"> 
+            <div class = "title">
+                <h2>Get in Touch</h2>
+            </div>
+
+            <div class = "box"> 
+                <!-- contact form -->
+                <div class="contact form"> 
+                    <h3> Send us a message</h3>
+                    <form>
+                        <div class="formBox">
+                            <div class="row50">
+                                <div class="inputBox">
+                                    <span>First Name</span>   
+                                    <input type="text">                                 
+                                </div>
+
+                                
+                                <div class="inputBox">
+                                    <span>Last Name</span>   
+                                    <input type="text">                                 
+                                </div>
+                            </div>
+
+                            <div class="row50">
+                                <div class="inputBox">
+                                    <span>Email</span>   
+                                    <input type="text">                                 
+                                </div>
+
+                                <div class="inputBox">
+                                    <span>Mobile</span>   
+                                    <input type="text">                                 
+                                </div>
+                            </div>
+
+                            <div class="row100">
+                                <div class="inputBox">
+                                    <span>Message</span>   
+                                    <textarea  ></textarea>                                
+                                </div>
+                            </div>
+                                <br>
+
+                            <div class="row100">
+                                <div class="inputBox">
+                                    <input type="submit" value = "Submit" class = "submit">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <!-- contact info -->
+                <div class="contact info"> 
+
+                </div>
+
+                <!-- map  -->
+                <div class="contact map"> 
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- end of section-contact  -->
 
 </body>
 
+
 <footer>
-    <h3>Contact Us</h3>
+
 </footer>
 </html>
